f5e261742c09390eef25240c6d68e18141de43d8
FaisalAJ88 <faisaljayadi88@gmail.com>
2025-03-01 22:45:22 +0700

    save

--- tests/MyConnect.spec.ts
+++ tests/MyConnect.spec.ts
@@ -1,23 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('Login Saucedemo', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com');
-  await page.getByRole('textbox', {name:'Username'}).fill('visual_user');
-  await page.getByRole('textbox', {name:'Password'}).fill('secret_sauce');
-  await page.screenshot({ path: 'login_page1.png' });
-  await page.getByRole('button', {name:'Login'}).click();
-  await page.waitForTimeout(2000);
-  
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-  await page.waitForTimeout(2000);
-  await page.screenshot({ path: 'dashboard_page1.png' });
+test('Login Saucedemo with valid credential', async ({ page }) => {
 
-  await page.keyboard.press('PageDown'); // Scroll turun 1 halaman
+  // this function to open web saucedemo.com
+  await page.goto('https://www.saucedemo.com');
+
+  // this function to find element field Username and fill the text box
+  await page.getByRole('textbox', {name:'Username'}).fill('visual_user');
+
+  // this function to find element field Password and fill the text box
+  await page.getByRole('textbox', {name:'Password'}).fill('secret_sauce');
+  
+  // this function to find element field Login Button and click that button
+  await page.getByRole('button', {name:'Login'}).click();
+  
+  // this function to make timeout in 2 second
   await page.waitForTimeout(2000);
-  await page.screenshot({ path: 'scrool_page1.png' });
+
+  // this assertion if user valid credential will be get this URL
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html'); 
+
+  // this function to make screenshot with name 
+  await page.screenshot({ path: 'successlogin_saucedemo.png' });
+
 });
 
-test.skip('Register', async ({ page }) => {
+test('Register', async ({ page }) => {
   await page.goto('https://www.doitpay.co/en');
   await expect(page.locator('//nav[@class="mx-auto flex max-w-7xl items-center justify-between px-6 md:px-8 py-4"]//img[@alt="Doitpay"]')).toBeVisible();
   await page.waitForTimeout(2000);
