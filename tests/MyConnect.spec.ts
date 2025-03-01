e83d06d281b8a79f90ba0691e6ab5c7fdde4831e
FaisalAJ88 <faisaljayadi88@gmail.com>
2025-03-01 22:47:59 +0700

    modify

--- tests/MyConnect.spec.ts
+++ tests/MyConnect.spec.ts
@@ -25,15 +25,27 @@ test('Login Saucedemo with valid credential', async ({ page }) => {
 
 });
 
-test('Register', async ({ page }) => {
-  await page.goto('https://www.doitpay.co/en');
-  await expect(page.locator('//nav[@class="mx-auto flex max-w-7xl items-center justify-between px-6 md:px-8 py-4"]//img[@alt="Doitpay"]')).toBeVisible();
-  await page.waitForTimeout(2000);
-  await page.screenshot({ path: 'dashboard_page.png' });
+test.skip('Login Saucedemo with valid credential', async ({ page }) => {
 
-  page.getByRole('link', { name: 'Get started free' }).click();
-  await expect(page).toHaveURL('https://dashboard.doitpay.co/register');
+  // this function to open web saucedemo.com
+  await page.goto('https://www.saucedemo.com');
+
+  // this function to find element field Username and fill the text box
+  await page.getByRole('textbox', {name:'Username'}).fill('visual_user');
+
+  // this function to find element field Password and fill the text box
+  await page.getByRole('textbox', {name:'Password'}).fill('secret_sauce123');
+  
+  // this function to find element field Login Button and click that button
+  await page.getByRole('button', {name:'Login'}).click();
+  
+  // this function to make timeout in 2 second
   await page.waitForTimeout(2000);
-  await page.screenshot({ path: 'register_page.png' });
+
+  // this assertion if user valid credential will be get this URL
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html'); 
+
+  // this function to make screenshot with name 
+  await page.screenshot({ path: 'successlogin_saucedemo.png' });
 
 });
