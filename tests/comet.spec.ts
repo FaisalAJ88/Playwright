79264b81fa8e8240fff0eb90cfcaaddd5e1e5a8d
FaisalAJ88 <faisaljayadi88@gmail.com>
2025-03-20 15:50:12 +0700

    update

--- tests/comet.spec.ts
+++ tests/comet.spec.ts
@@ -1,24 +1,24 @@
 import { test, expect } from '@playwright/test'; 
 
-test('Login Comet Staging', async ({ page }) => {
+test('Login OWS Staging', async ({ page }) => {
 
   // this function to open web saucedemo.com
-  await page.goto('https://ftth-sit-aws-comet.xl.co.id/login');
+  await page.goto('https://107d-sg-studio.teleows.com/');
 
   // this function to find element field Username and fill the text box
-  await page.getByRole('button', {name:'Login with O365'}).click();
+  await page.fill('#username', 'JKTS-DSL-001');
 
   // this function to find element field Password and fill the text box
-  await expect(page).toHaveURL('https://ftth-sit-aws-comet.xl.co.id/%23/landing');
+  await page.fill('#password', 'y12345!!');
   
   // this function to make timeout in 2 second
   await page.waitForTimeout(2000);
 
   // this function to find element field Login Button and click that button
-  //await page.getByRole('button', {name:'Login'}).click();
+  await page.click('#loginButton');
   
   // this assertion if user invalid credential will be get some failed respone
-  //await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
+  await expect(page.getByText('User Basic Info')).toBeVisible();
 
   // this function to make screenshot with name 
   //await page.screenshot({ path: 'failedlogin_username.png' });
