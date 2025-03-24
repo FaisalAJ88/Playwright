ae2123f088e9f6c81b6317d374f7d249860e460d
FaisalAJ88 <faisaljayadi88@gmail.com>
2025-03-24 10:54:52 +0700

    updated

--- tests/comet.spec.ts
+++ tests/comet.spec.ts
@@ -6,10 +6,10 @@ test('Login OWS Staging', async ({ page }) => {
   await page.goto('https://107d-sg-studio.teleows.com/');
 
   // this function to find element field Username and fill the text box
-  await page.fill('#username', 'test_helpdesk_sales');
+  await page.fill('//input[@id="username"]', 'test_helpdesk_sales');
 
   // this function to find element field Password and fill the text box
-  await page.fill('#password', 'qwerty12345!!');
+  await page.fill('//input[@id="password"]', 'qwerty12345!!');
   
   // this function to make timeout in 2 second
   await page.waitForTimeout(2000);
@@ -20,9 +20,4 @@ test('Login OWS Staging', async ({ page }) => {
   // this assertion if user invalid credential will be get some failed respone
   await expect(page.getByText('//div[@title="test_helpdesk_sales"]')).toBeVisible();
 
-  // this function to make screenshotwith name 
-  //await page.screenshot({ path: 'failedlogin_username.png' });
-
-  // this function to make timeout in 2 second
-  //await page.waitForTimeout(2000);
 });
