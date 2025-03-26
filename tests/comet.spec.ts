f475faa2b318ba0001d64c98c4eaf5f56a9e8cbb
FaisalAJ88 <faisaljayadi88@gmail.com>
2025-03-26 13:40:50 +0700

    update

--- tests/comet.spec.ts
+++ tests/comet.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Login ORANGE HRM DEMO', async ({ page }) => {
 
-  // this function to open web saucedemo.com
+  // this function to open web opensource demo orange HRM
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
   // this function to find element field Username and fill the text box
@@ -31,3 +31,28 @@ test('Login ORANGE HRM DEMO', async ({ page }) => {
   test.info().attach('dashboard_page', {contentType: 'image/png', body: screenshot1});
 
 });
+
+test('Forgot Password', async ({ page })=> {
+  
+  // this function to open web opensource demo orange HRM
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+  // click text button forgot email
+  await page.click('.orangehrm-login-forgot-header')
+  await page.waitForTimeout(2000);
+
+  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode');
+  await page.waitForTimeout(2000);
+
+  // input textbox username
+  await page.getByRole('textbox', {name:'Username'}).fill('Test123')
+  await page.waitForTimeout(2000);
+
+  // save screenshot
+  const screenshot2 = await page.screenshot()
+  test.info().attach('Reset_Password', {contentType: 'image/png', body: screenshot2});
+  
+  // Click button Reset Password
+  await page.getByRole('button', {name:'Reset Password'}).click()
+
+})
