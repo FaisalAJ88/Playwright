0e924db9d105031f1af4212cf48130b44b5e244c
FaisalAJ88 <faisaljayadi88@gmail.com>
2025-03-26 15:05:38 +0700

    update

--- tests/orangehrm.spec.ts
+++ tests/orangehrm.spec.ts
@@ -84,7 +84,7 @@ test.only('TC 04: Check Link Youtube', async ({ page })=> {
   await page.locator('[href="https://www.youtube.com/c/OrangeHRMInc"] > .oxd-icon').click()
   await page.waitForTimeout(3000);
   // assertion URL Link Youtube website
-  await expect(page.getByText('OrangeHRM Inc')).toBeVisible()
+  await expect(page.getByText('/OrangeHRM Inc/')).toBeEnabled()
   await page.waitForTimeout(2000);
   // save screenshot
   const screenshot4 = await page.screenshot()
