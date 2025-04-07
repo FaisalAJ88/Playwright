b4b95d750fa6cd43b78446e43e57579e06aea1b5
FaisalAJ88 <faisaljayadi88@gmail.com>
2025-04-07 11:00:51 +0700

    updated

---
+++ tests/orangehrm_checkdashboardmenu.spec.ts
@@ -0,0 +1,39 @@
+test('TC 01: Dashboard_Check_Menu_Admin', async ({ page }) => {
+
+  // this function to open web opensource demo orange HRM
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+  // this function to find element field Username and fill the text box
+  await page.getByRole('textbox', {name : 'Username'}).fill('Admin')
+
+  // this function to find element field Password and fill the text box
+  await page.getByRole('textbox', {name : 'Password'}).fill('admin123')
+  
+  // this function to make timeout in 2 second
+  await page.waitForTimeout(2000);
+
+  // take screenshot
+  const screenshot = await page.screenshot()
+  test.info().attach('login_page', {contentType: 'image/png', body: screenshot});
+  // this function to find element field Login Button and click that button
+  await page.getByRole('button', {name : 'Login'}).click()
+  
+  // this assertion Text Dashboard in dashboard_page
+  await expect(page.locator('.oxd-text--h6')).toBeVisible();
+  //take timeout
+  await page.waitForTimeout(2000);
+
+  //take screenshot Dashboard
+  const screenshot1 = await page.screenshot()
+  test.info().attach('dashboard_page', {contentType: 'image/png', body: screenshot1});
+
+  // this function to find element field Menu Admin
+  await page.getByRole('link', {name : 'Admin'}).click()
+  //take timeout
+  await page.waitForTimeout(2000);
+
+  //take screenshot Dashboard
+  const screenshot2 = await page.screenshot()
+  test.info().attach('Menu_Admin', {contentType: 'image/png', body: screenshot2});
+
+})
