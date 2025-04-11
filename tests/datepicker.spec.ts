e4c21c9bb2266724b8df9076d58c014c1f700f47
FaisalAJ88 <faisaljayadi88@gmail.com>
2025-04-11 11:22:33 +0700

    update

--- tests/datepicker.spec.ts
+++ tests/datepicker.spec.ts
@@ -4,10 +4,17 @@ test('TC 01: Handle Datepicker', async ({ page }) => {
 
   // this function to open web opensource demo orange HRM
   await page.goto('https://testautomationpractice.blogspot.com');
-
+  await page.waitForTimeout(2000);
+  await page.keyboard.press('PageDown');
+  await page.waitForTimeout(2000);
+  await page.keyboard.press('PageDown');
   // this function to find element field Username and fill the text box
   await page.fill('#datepicker','03/15/2024')
-
+  
+  while(true)
+  {
+    const currentYear=await page.locator()
+  }
   // this function to make timeout in 2 second
   await page.waitForTimeout(2000);
 
