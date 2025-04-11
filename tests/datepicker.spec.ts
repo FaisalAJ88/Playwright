35a41b4d7438e157ead7e6fae0bdd9d7a5cfc223
FaisalAJ88 <faisaljayadi88@gmail.com>
2025-04-11 10:32:18 +0700

    updated

---
+++ tests/datepicker.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect, chromium, firefox, webkit } from '@playwright/test'; 
+
+test('TC 01: ', async ({ page }) => {
+
+  // this function to open web opensource demo orange HRM
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+  // this function to find element field Username and fill the text box
+  await page.getByRole('textbox', {name : 'Username'}).fill('Admin')
+
+  // this function to find element field Password and fill the text box
+  await page.getByRole('textbox', {name : 'Password'}).fill('admin123')
+  
+  // this function to make timeout in 2 second
+  await page.waitForTimeout(2000);
+
+  // take screenshot
+  const screenshot = await page.screenshot()
+  test.info().attach('login_page', {contentType: 'image/png', body: screenshot});
+  // this function to find element field Login Button and click that button
+  await page.getByRole('button', {name : 'Login'}).click()
+  
+  // this assertion Text Dashboard in dashboard_page
+  await expect(page.locator('.oxd-text--h6')).toBeVisible();
+
+  //take timeout
+  await page.waitForTimeout(2000);
+
+  //take screenshot
+  const screenshot1 = await page.screenshot()
+  test.info().attach('dashboard_page', {contentType: 'image/png', body: screenshot1});
+
+})
