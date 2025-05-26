e6bc5bdae9b6cabed60bdf6aedbb01e84d87469f
FaisalAj88 <faisaljayadi88@gmail.com>
2025-05-26 15:16:21 +0700

    updated

---
+++ tests/datepickerthird.spc.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://only-testing-blog.blogspot.com/2025/04/alert-dialogs.html');
+  //Click on date input field to open date picker.
+  await page.locator('.flatpickr-input').click();  
+  
+  // Navigate to specific year.
+  while (await page.getByRole('spinbutton', { name: 'Year' }).inputValue() !== '2027') {
+    await page.locator('.arrowUp').click();
+    await page.waitForTimeout(1000);
+  }
+  //Select specific month
+  await page.getByLabel('Month').selectOption('September');
+  await page.waitForTimeout(1000);
+
+  //Select specific date
