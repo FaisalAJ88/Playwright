ddea497ad4268643f73f43cbb4a64a90fe520485
Faisal <faisaljayadi88@gmail.com>
2025-06-16 17:10:20 +0700

    COMMIT

--- tests/usedotenv.spec.ts
+++ tests/usedotenv.spec.ts
@@ -2,25 +2,27 @@ import { expect, test } from '@playwright/test'
 import dotenv from 'dotenv';
 dotenv.config();
 const baseURL = process.env.baseURL as string;
-const username = process.env.username as string;
-const password = process.env.password as string;
+const username1 = process.env.username1 as string;
+const password1 = process.env.password1 as string;
 
 test('Orange HRM login', async ({ page }) => {
 
   await page.goto(baseURL);
 
   await page.waitForTimeout(5000);
-  
-  await page.getByRole('textbox', {name : 'Username'}).fill(username);
-  await page.getByRole('textbox', {name : 'Password'}).fill(password);
+  console.log(process.env.username1)
+  console.log(process.env.password1)
 
-  await page.waitForTimeout(2000);
+  //await page.getByRole('textbox', {name : 'Username'}).fill(username1);
+  //await page.getByRole('textbox', {name : 'Password'}).fill(password1);
 
-  await page.getByRole('button', {name : 'Login'}).click()
+  //await page.waitForTimeout(2000);
+
+  //await page.getByRole('button', {name : 'Login'}).click()
     
     // this assertion Text Dashboard in dashboard_page
-    await expect(page.locator('.oxd-text--h6')).toBeVisible();
-  
+    //await expect(page.locator('.oxd-text--h6')).toBeVisible();
+   
     //take timeout
     //await page.waitForTimeout(2000);
 
