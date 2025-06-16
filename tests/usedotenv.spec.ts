6599862bfdaa94396d0bb8f6566e86cf2b4d4cac
Faisal <faisaljayadi88@gmail.com>
2025-06-16 11:15:56 +0700

    add dotenv test case

---
+++ tests/usedotenv.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '@playwright/test'
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Read from ".env" file.
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+const baseURL = process.env.baseURL as string;
+const username = process.env.username as string;
+const password = process.env.password as string;
+
+test('Google login', async ({ page }) => {
+  await page.goto(baseURL);
+
+  await page.getByRole('textbox', {name : 'Username'}).fill(username)
+  await page.getByRole('textbox', {name : 'Password'}).fill(password)
+
+  await page.waitForTimeout(2000);
+
+  await page.getByRole('button', {name : 'Login'}).click()
+    
+    // this assertion Text Dashboard in dashboard_page
+    await expect(page.locator('.oxd-text--h6')).toBeVisible();
+  
+    //take timeout
+    await page.waitForTimeout(2000);
+
+    //take screenshot
+  const screenshot1 = await page.screenshot()
+  test.info().attach('dashboard_page', {contentType: 'image/png', body: screenshot1});
+
+})
