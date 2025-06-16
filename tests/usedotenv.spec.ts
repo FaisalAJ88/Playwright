9e70e006f9adaffb656265e022619b1e899dc2ef
Faisal <faisaljayadi88@gmail.com>
2025-06-16 14:17:19 +0700

    ok

--- tests/usedotenv.spec.ts
+++ tests/usedotenv.spec.ts
@@ -8,18 +8,18 @@ test('Orange HRM login', async ({ page }) => {
 
   await page.goto(baseURL);
 
-  await page.getByRole('textbox', {name : 'Username'}).fill(username)
-  await page.getByRole('textbox', {name : 'Password'}).fill(password)
+  //await page.getByRole('textbox', {name : 'Username'}).fill(username);
+  //await page.getByRole('textbox', {name : 'Password'}).fill(password);
 
   await page.waitForTimeout(2000);
 
-  await page.getByRole('button', {name : 'Login'}).click()
+  //await page.getByRole('button', {name : 'Login'}).click()
     
     // this assertion Text Dashboard in dashboard_page
-    await expect(page.locator('.oxd-text--h6')).toBeVisible();
+    //await expect(page.locator('.oxd-text--h6')).toBeVisible();
   
     //take timeout
-    await page.waitForTimeout(2000);
+    //await page.waitForTimeout(2000);
 
     //take screenshot
   const screenshot1 = await page.screenshot()
