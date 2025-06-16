5367c019bff745e9c3e274df92a3ecd2b2a95742
Faisal <faisaljayadi88@gmail.com>
2025-06-16 14:29:13 +0700

    change elemet find

--- tests/usedotenv.spec.ts
+++ tests/usedotenv.spec.ts
@@ -8,12 +8,12 @@ test('Orange HRM login', async ({ page }) => {
 
   await page.goto(baseURL);
 
-  //await page.getByRole('textbox', {name : 'Username'}).fill(username);
-  //await page.getByRole('textbox', {name : 'Password'}).fill(password);
+  await page.getByRole('textbox', {name : 'Username'}).fill(username);
+  await page.getByRole('textbox', {name : 'Password'}).fill(password);
 
   await page.waitForTimeout(2000);
 
-  //await page.getByRole('button', {name : 'Login'}).click()
+  await page.getByRole('button', {name : 'Login'}).click()
     
     // this assertion Text Dashboard in dashboard_page
     //await expect(page.locator('.oxd-text--h6')).toBeVisible();
