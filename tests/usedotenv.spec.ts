1cb16b1eb4d40f1cac26364ae5098620ddd11923
Faisal <faisaljayadi88@gmail.com>
2025-06-16 13:09:46 +0700

    add

--- tests/usedotenv.spec.ts
+++ tests/usedotenv.spec.ts
@@ -1,19 +1,19 @@
 import { expect, test } from '@playwright/test'
 
 const baseURL = process.env.baseURL as string;
-//const username = process.env.username as string;
-//const password = process.env.password as string;
+const username = process.env.username as string;
+const password = process.env.password as string;
 
 test('Orange HRM login', async ({ page }) => {
 
   await page.goto(baseURL);
 
-  //await page.getByRole('textbox', {name : 'Username'}).fill(username)
-  //await page.getByRole('textbox', {name : 'Password'}).fill(password)
+  await page.getByRole('textbox', {name : 'Username'}).fill(username)
+  await page.getByRole('textbox', {name : 'Password'}).fill(password)
 
   await page.waitForTimeout(2000);
 
-  //await page.getByRole('button', {name : 'Login'}).click()
+  await page.getByRole('button', {name : 'Login'}).click()
     
     // this assertion Text Dashboard in dashboard_page
     await expect(page.locator('.oxd-text--h6')).toBeVisible();
